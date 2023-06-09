7a5442279a50b336396df8f0a2e13efb59341946
thioro faye <fayethioro1998@gmail.com>
2023-06-09 12:43:32 +0000

    discipline version 2

---
+++ views/classe/ponderation.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="/../../css/annee.css">
+<link rel="stylesheet" href="/../../css/niveau.css">
+<link rel="stylesheet" href="/../../css/style.css">
+
+<div class="contenainer dflex fdc">
+    <main>
+
+
+    </main>
+</div>
+<script src="/../../js/annee.js"></script>
